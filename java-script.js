760dcce9a26dce5610785c437be92e965bcfc876
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-02-17 04:29:27 +0600

    js part done without error handling

--- java-script.js
+++ java-script.js
@@ -1,13 +1,36 @@
+// input field value receiving function
+
 function gettingInputValue(value){
     const inputValue = document.getElementById(value+'-input').value;
     const integerValue = parseInt(inputValue);
     return integerValue;
 }
+// function for showing the amounts in html
+function showingBalance(elementId,amount){
+    document.getElementById(elementId).innerText = amount;
+}
+// Calculate Button Event
 document.getElementById('calculate-button').addEventListener('click',function(){
+    // taking integer input of input fields
     const incomeAmount = gettingInputValue('income');
-    console.log(incomeAmount);
     const foodAmount = gettingInputValue('food');
-    console.log(foodAmount);
     const rentAmount = gettingInputValue('rent');
-    console.log(rentAmount);
+    const clothesAmount = gettingInputValue('clothes');
+    const totalExpenses = foodAmount + rentAmount + clothesAmount;
+    const balance = incomeAmount - totalExpenses; 
+    // showing total expenses & balance in html 
+    showingBalance('total-expenses',totalExpenses);
+    showingBalance('balance',balance);
+
+})
+
+// Save Button Event 
+document.getElementById('save-button').addEventListener('click',function(){
+    const savePercentage = gettingInputValue('save');
+    const incomeAmount = gettingInputValue('income');
+    const saveAmount =Math.round((savePercentage/100) * incomeAmount); 
+    showingBalance('saving-amount',saveAmount);
+    const balance = parseInt(document.getElementById('balance').innerText)
+    const remainingBalance = balance - saveAmount;
+    showingBalance('remaining-balance',remainingBalance);
 })
