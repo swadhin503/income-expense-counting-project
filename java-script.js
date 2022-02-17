f21b927b637e190505a6415fcdc15eecc6aa9e4a
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-02-17 22:02:38 +0600

    error handling half done

--- java-script.js
+++ java-script.js
@@ -1,12 +1,17 @@
 // input field value receiving function
-
 function gettingInputValue(value){
+//    const error = parseInt(value);
+  
     const inputValue = document.getElementById(value+'-input').value;
     const integerValue = parseInt(inputValue);
-    return integerValue;
+        return integerValue;
 }
 // function for showing the amounts in html
 function showingBalance(elementId,amount){
+    // if(isNaN(amount)){
+    //     document.getElementById('warning-message').style.display=("block");
+    // }
+    // else
     document.getElementById(elementId).innerText = amount;
 }
 // Calculate Button Event
@@ -15,12 +20,25 @@ document.getElementById('calculate-button').addEventListener('click',function(){
     const incomeAmount = gettingInputValue('income');
     const foodAmount = gettingInputValue('food');
     const rentAmount = gettingInputValue('rent');
-    const clothesAmount = gettingInputValue('clothes');
-    const totalExpenses = foodAmount + rentAmount + clothesAmount;
-    const balance = incomeAmount - totalExpenses; 
+    const clothesAmount = gettingInputValue('clothes'); 
     // showing total expenses & balance in html 
-    showingBalance('total-expenses',totalExpenses);
-    showingBalance('balance',balance);
+    if (isNaN(incomeAmount) || isNaN(foodAmount) || isNaN(rentAmount) || isNaN(clothesAmount)){
+        document.getElementById('warning-message').style.display=("block");
+    }
+    else if (incomeAmount<0 || foodAmount<0 || rentAmount<0 || clothesAmount<0){
+        document.getElementById('warning-message2').style.display=("block");
+    }
+    else{ 
+        const totalExpenses = foodAmount + rentAmount + clothesAmount;
+        const balance = incomeAmount - totalExpenses;
+        if (totalExpenses > incomeAmount) {
+            document.getElementById('error-message').style.display=("block");
+        }  
+        showingBalance('total-expenses',totalExpenses);
+        showingBalance('balance',balance);
+        document.getElementById('warning-message').style.display=("none");
+        document.getElementById('warning-message2').style.display=("none");
+    }
 
 })
 
@@ -28,9 +46,19 @@ document.getElementById('calculate-button').addEventListener('click',function(){
 document.getElementById('save-button').addEventListener('click',function(){
     const savePercentage = gettingInputValue('save');
     const incomeAmount = gettingInputValue('income');
-    const saveAmount =Math.round((savePercentage/100) * incomeAmount); 
-    showingBalance('saving-amount',saveAmount);
-    const balance = parseInt(document.getElementById('balance').innerText)
-    const remainingBalance = balance - saveAmount;
-    showingBalance('remaining-balance',remainingBalance);
+    if(isNaN(savePercentage)){
+        document.getElementById('warning-message3').style.display=("block");
+    }
+    else if(savePercentage < 0){
+        document.getElementById('warning-message4').style.display=("block");
+    }
+    else {
+        const saveAmount =Math.round((savePercentage/100) * incomeAmount);
+        document.getElementById('warning-message3').style.display=("none");
+        document.getElementById('warning-message4').style.display=("none");
+        showingBalance('saving-amount',saveAmount);
+        const balance = parseInt(document.getElementById('balance').innerText)
+        const remainingBalance = balance - saveAmount;
+        showingBalance('remaining-balance',remainingBalance);
+    }
 })
